ddb6812c5449f792450fbdc82532ac7cb6b29ada
sarthak-env <sarthak.devlog@gmail.com>
2026-08-30 01:51:47 +0530

    Add first C++ program, update gitignore and README

---
+++ cpp_fundamentals/first_program.cpp
@@ -0,0 +1,5 @@
+#include<iostream> // this line includes the input-output stream library, which allows us to use std::cout for printing to the console
+int main() {
+    std :: cout<<"Hello World!, this is my first program in C++"; // prints the message "Hello, World!" to the console
+    return 0; // this line indicates that the program has executed successfully
+}
