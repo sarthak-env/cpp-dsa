b646f1fb054de309995425b65b9724ef964a5c19
sarthak-env <sarthak.devlog@gmail.com>
2026-08-30 14:10:15 +0530

    Add basic C++ program demonstrating variables and comments

---
+++ cpp_fundamentals/variables_n_comments.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age=21; //This stores the age of the person
+    /*
+    We can change the value of age later in the program
+    whenever we need to.
+   */
+    age=22; //This changes the value of age to 22
+    cout << "Your age is:  " << age; // This prints the age of the person to the console
+    return 0;       
+}
+
+/* 
+Note:
+- Comments are used to explain the code and make it easier to understand
+- Single-line comments are used to write short explanations on one line
+- Multi-line comments are used to write longer explanations that span multiple lines
+*/
