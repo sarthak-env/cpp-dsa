bc23392a9aa4ce513b47b2365ebfb3a8d3bdcc43
sarthak-env <sarthak.devlog@gmail.com>
2026-08-30 14:37:37 +0530

    Add basic C++ program demonstrating data types

---
+++ cpp_fundamentals/04_datatypes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 21;  // int is used to store whole numbers
+    float marks = 85.5; // float is used to store decimal numbers
+    double price = 99.99; // double is used to store decimal numbers with more precision
+    char grade = 'A'; // char is used to store a single character
+    bool isStudent = true;  // bool is used to store true or false values
+    string name = "Sarthak"; // string is used to store text
+
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Marks: " << marks << endl;
+    cout << "Price: " << price << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Student: " << isStudent << endl;
+
+    return 0;
+}
+
+// Here, endl means end line. It moves the cursor to the next line after printing
