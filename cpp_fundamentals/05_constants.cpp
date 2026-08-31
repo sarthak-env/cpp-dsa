4a386bf4a7d0e9bc9bc136cba683d4c9719b8c84
sarthak-env <sarthak.devlog@gmail.com>
2026-08-31 19:19:17 +0530

    Add basic C++ program demonstrating constants

---
+++ cpp_fundamentals/05_constants.cpp
@@ -0,0 +1,12 @@
+// Constants in C++
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Defining a constant variable
+    const double PI = 3.14159;  // Trying to modify the constant will result in a compilation error
+   
+    cout << "The value of PI is: " << PI << endl;
+    return 0;
+}
