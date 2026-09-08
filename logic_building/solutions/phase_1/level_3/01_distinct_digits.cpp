c21ef702b0e299251ae6cc17eb63e688a567953c
sarthak-env <sarthak.devlog@gmail.com>
2026-09-08 19:58:37 +0530

    solved 1.3.1 — check if all digits of a 3-digit number are distinct

---
+++ logic_building/solutions/phase_1/level_3/01_distinct_digits.cpp
@@ -0,0 +1,28 @@
+// Problem: Take a 3-digit number and check if all digits are distinct
+// Phase: 1 - Conditionals
+// Level: 3 [Q1]
+
+#include<iostream>
+#include <cctype>
+using namespace std;
+
+int main(){
+    string number_three_digits;
+    cout<<"Enter the three numbers:"<<endl;
+    cin>>number_three_digits;
+
+    if(number_three_digits.length()==3 &&  isdigit(number_three_digits[0]) && isdigit(number_three_digits[1]) && isdigit(number_three_digits[2])){
+        if( number_three_digits[0] != number_three_digits[1] && number_three_digits[0] != number_three_digits[2] && number_three_digits[1] != number_three_digits[2]){
+            cout<<"All three digiits are distinct (different)"<<endl;
+        }
+        else{
+            cout<<"All the three digits are not distinct"<<endl;
+        }
+    }
+    else{
+        cout<<"Enter a three digit number:"<<endl;
+    }
+
+    return 0;
+
+}
