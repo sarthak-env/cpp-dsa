c6a94ac8a54a0cf8b48317be0995c2b8d15f6e68
sarthak-env <sarthak.devlog@gmail.com>
2026-09-15 19:23:43 +0530

    solved 2.1.3 — print all odd numbers between 1 and 100

---
+++ logic_building/solutions/phase_2/level_1/03_print_odd_1_to_100.cpp
@@ -0,0 +1,14 @@
+// Problem: Print all odd numbers between 1 and 100.
+// Phase: 2 - Loops & Patterns
+// Level: 1 [Q3]
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for(int i=1; i<=100; i+=2){
+        cout<<i<<" "<<endl;
+    }
+
+    return 0;
+}
